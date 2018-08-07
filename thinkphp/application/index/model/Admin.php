3abec0dd1adeb0e2f27847bc1ce5f779bfaadeae
chenyunxing <807385017@qq.com>
2018-08-07 12:37:44 +0800

    7

--- thinkphp/application/index/model/Admin.php
+++ thinkphp/application/index/model/Admin.php
@@ -1,8 +1,9 @@
-<?php 
+<?php
 namespace app\index\model;
 use think\Model;
 class Admin extends Model
 {
+	//检查用户名和密码是否存在数据库，并且只有一条
 	public function check($username,$password){
 		$data['username']=$username;
 		$data['password']=$password;
